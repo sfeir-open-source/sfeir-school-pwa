5f9788b2bef455813cc17b7e775d60ea1f19dd6b
jefbinomed <jean.francois.garreau@gmail.com>
2020-09-02 18:00:33 +0200

    feat: add solution for background sync exercice

---
+++ steps/sync-1-solution/app/services/People.js
@@ -0,0 +1,70 @@
+import { longPolling } from './longpolling-helper.js';
+
+export class PeoplesService {
+  constructor() {
+    const SERVER = 'http://localhost:3000';
+    this.API_URL = `${SERVER}/people`;
+    this.peoples = null;
+    this.peopleMap = new Map();
+    this.hasRequestPending = false;
+    this.networkPromise = this.initialize();
+  }
+
+  async initialize() {
+    this.hasRequestPending = true;
+    const response = await fetch(this.API_URL);
+    const data = await response.json();
+    this.hasRequestPending = false;
+    if (this.peoples) {
+      //Replace this.peoples
+      Array.prototype.splice.apply(this.peoples, [0, this.peoples.length].concat(data));
+    } else {
+      this.peoples = data;
+    }
+  }
+
+  onResult() {
+    this.peoples.forEach(people => {
+      people.name = people.firstname + ' ' + people.lastname;
+      this.peopleMap.set(people.email, people);
+    });
+    return this.peoples;
+  }
+
+  async getPeoples() {
+    await this.networkPromise;
+    return this.onResult();
+  }
+
+  getPeopleById(id) {
+    return this.peopleMap.get(id);
+  }
+
+  async updatePeope(people) {
+    const options = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(people)
+    };
+    try {
+      return await fetch(`${this.API_URL}/${people.id}`, options);
+    } catch (e) {
+      longPolling(`${this.API_URL}/${people.id}`, options);
+    }
+  }
+
+  getCollab(email) {
+    var response = { isManager: false, collab: [] };
+    angular.forEach(
+      this.peopleMap,
+      function(value, key) {
+        if (value.manager === email) {
+          response.isManager = true;
+          response.collab.push(key);
+        }
+      },
+      response
+    );
+    return response;
+  }
+}
---
+++ steps/sync-1-solution/service-worker.js
@@ -0,0 +1,115 @@
+console.log('Service worker ok =D');
+self.importScripts('https://cdn.jsdelivr.net/npm/idb@4.0.5/build/iife/with-async-ittr-min.js');
+
+const cacheAppShellStatic = [
+  '/',
+  'mocks/notification.json',
+  //'http://localhost:3000/people',
+  '/index.html',
+  '/css/app.css',
+  '/img/bg_left.png',
+  '/img/bg_right.png',
+  '/img/cat.jpg',
+  '/img/logo-sfeir.svg',
+  '/img/offline.jpg',
+  '/img/search.svg',
+  '/mdl/material.min.css',
+  '/mdl/material.min.js',
+  '/css/material-icons.css',
+  '/css/md-overwrite.css',
+  '/css/font/MaterialIcons-Regular.woff2'
+];
+
+self.addEventListener('install', event => {
+  console.log('event install');
+  event.waitUntil(
+    caches
+      .open('cache-static')
+      .then(cache => cache.addAll(cacheAppShellStatic))
+      .then(_ => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('event activate');
+  event.waitUntil(self.clients.claim().then(_ => caches.delete('cache-dynamic')));
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  const catImage = 'img/cat.jpg';
+  const offlineFile = 'offline.html';
+
+  if (url.pathname.includes('socket.io') || url.origin.startsWith('chrome-extension')) {
+    return false;
+  } else if (url.port === '3000') {
+    return fetch(event.request);
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return (
+          response ||
+          fetch(event.request)
+            .then(responseFetch =>
+              caches.open('cache-dynamic').then(cache => {
+                cache.put(event.request, responseFetch.clone());
+                return responseFetch;
+              })
+            )
+            .catch(_ => event.respondWith(caches.match(new Request(offlineFile))))
+        );
+      })
+    );
+  }
+});
+
+function upgrade(db) {
+  db.createObjectStore('request');
+}
+
+async function cleanRequest(id) {
+  const db = await idb.openDB('failedRequestDB', 1, {
+    upgrade: upgrade
+  });
+
+  await db.delete('request', id);
+}
+
+async function getFailedRequest() {
+  const db = await idb.openDB('failedRequestDB', 1, {
+    upgrade: upgrade
+  });
+
+  let cursor = await db.transaction('request').store.openCursor();
+
+  const requestArray = [];
+  while (cursor) {
+    console.log(cursor.key, cursor.value);
+    requestArray.push({ id: cursor.key, value: cursor.value });
+    cursor = await cursor.continue();
+  }
+  return requestArray;
+}
+
+self.addEventListener('sync', async function(event) {
+  console.log('sync event', event);
+  if (event.tag.startsWith('testSync')) {
+    console.log('testSync event recieved');
+    event.waitUntil(
+      new Promise(async (resolve, reject) => {
+        console.log('get Failed Request');
+        const requestArray = await getFailedRequest();
+        if (requestArray) {
+          for (let request of requestArray) {
+            try {
+              await fetch(request.value.url, request.value.options);
+              await cleanRequest(request.id);
+            } catch (e) {
+              console.error(e);
+            }
+          }
+        }
+      })
+    );
+  }
+});
