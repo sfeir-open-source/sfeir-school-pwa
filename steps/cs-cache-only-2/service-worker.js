b616ce7929ca9209ded80c631f6aa8d3d8a1fb6b
jefbinomed <jean.francois.garreau@gmail.com>
2022-09-19 17:54:26 +0200

    exercices: update workbox version for samples

--- steps/cs-cache-only-2/service-worker.js
+++ steps/cs-cache-only-2/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('http://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+importScripts('http://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 workbox.setConfig({
   debug: true
