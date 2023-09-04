83cedd51ff67b11a236e33b692a528f5b2e6b897
jefbinomed <jean.francois.garreau@gmail.com>
2023-09-04 17:58:02 +0200

    ex: add bases for "webotp" exercice

---
+++ steps/auth-webotp-1/app/index.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill';
+import { setupErrorListener } from '../../../common/utils/setupErrorListener';
+import '../../../common/app/components/HomeComponent';
+import '../../../common/app/components/PeopleCardComponent';
+import '../../../common/app/components/PeopleListComponent';
+import './components/LoginComponent';
+import '../../../common/app/components/PeopleApp';
+
+import 'normalize.css';
+import '../../../assets/css/app.css';
+
+setupErrorListener();
